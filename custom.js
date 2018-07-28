c895877cf557b6f59497ed5be29e0fcffc72a5e6
ualhalla <41804315+ualhalla@users.noreply.github.com>
2018-07-29 02:30:42 +0800

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,85 @@
+// footer
+function foot() {
+	var footSet = new Array();
+		footSet[1] = "http://xxx.mobilist.mobi/in/89384";
+		footSet[2] = "http://wap.toplistcreator.eu/in.php?nr=91";
+		footSet[3] = "http://toplistx.com/in/111671";
+		footSet[4] = "http://erolist.net/in/111676";
+		footSet[5] = "http://mtop.mobi/in/111700";
+		footSet[6] = "http://waptop.eu/in/111701";
+		footSet[7] = "http://onlyhits.mobilist.mobi/in/87864";
+		footSet[8] = "http://wap.topz.mobi/in/87865";
+		footSet[9] = "http://adzline.topz.mobi/in/87867";
+		footSet[10] = "http://loland.topz.mobi/in/87869";
+		footSet[11] = "http://top.mobilist.mobi/in/87871";
+		footSet[12] = "http://sextop.xxx/in/113584";
+		footSet[13] = "http://wapfun.eu/in/113586";
+		footSet[14] = "http://sextop.co/in/113593";
+		footSet[15] = "http://x2list.com/in/113595";
+		footSet[16] = "http://kickme.mobi/in/113597";
+		footSet[17] = "http://mobpath.com/in/111702";
+		footSet[18] = "http://sexlist.mobi/in/111703";
+		footSet[19] = "http://itech.topzone.mobi/in/89621";
+		footSet[20] = "http://xtlist.com/in/113599";
+		footSet[21] = "http://downloads.mobilist.mobi/in/89422";
+		footSet[22] = "http://charrua.mobilist.mobi/in/89448";
+		footSet[23] = "http://wap2.topz.mobi/in/89450";
+		footSet[24] = "http://porn.toplistcreator.eu/in.php?nr=239";
+		footSet[25] = "http://9ads.topz.mobi/in/89486";
+		footSet[26] = "http://hotgirls.mobilist.mobi/in/89492";
+		footSet[27] = "http://xnxx.toplistcreator.eu/in.php?nr=241";
+		footSet[28] = "http://mostlist.tophits.mobi/in/89507";
+		footSet[29] = "http://itech.topz.mobi/in/89607";
+		footSet[30] = "http://mp4x.mobilist.mobi/in/89513";
+		footSet[31] = "http://adzlist.topz.mobi/in/89543";
+		footSet[32] = "http://adzmobi.topz.mobi/in/89623";
+
+	document.write('<p style='+'"'+'text-align: left; font-family: verdana; font-size: 11px'+'"'+'>Partners: <a href='+'"'+footSet[1]+'"'+'>Download SEX Video!</a> &nbsp; <a href='+'"'+footSet[2]+'"'+'>XXX Download</a> &nbsp; <a href='+'"'+footSet[3]+'"'+'>18+ Videos</a> &nbsp; <a href='+'"'+footSet[4]+'"'+'>18+ MP4s</a> &nbsp; <a href='+'"'+footSet[5]+'"'+'>HOT! 18+</a> &nbsp; <a href='+'"'+footSet[6]+'"'+'>HOT Downloads! 18+</a> &nbsp; <a href='+'"'+footSet[7]+'"'+'>Free Downloading Site</a> &nbsp; <a href='+'"'+footSet[8]+'"'+'>Mobi Sex Videos 3GP MP4!</a> &nbsp; <a href='+'"'+footSet[9]+'"'+'>Free 18+ Sites!</a> &nbsp; <a href='+'"'+footSet[10]+'"'+'>Free Download Sites!</a> &nbsp; <a href='+'"'+footSet[11]+'"'+'>XXX Download Sites!</a> &nbsp; <a href='+'"'+footSet[12]+'"'+'>SexTop 18+</a> &nbsp; <a href='+'"'+footSet[13]+'"'+'>WAPFun</a> &nbsp; <a href='+'"'+footSet[14]+'"'+'>SexTOP 18+</a> &nbsp; <a href='+'"'+footSet[15]+'"'+'>x2List 18+</a> &nbsp; <a href='+'"'+footSet[16]+'"'+'>KICKme</a> &nbsp; <a href='+'"'+footSet[17]+'"'+'>MP4s M4Vs 18+</a> &nbsp; <a href='+'"'+footSet[18]+'"'+'>Free MP4s 3GPs</a> &nbsp; <a href='+'"'+footSet[19]+'"'+'>iTech TOPzone mobi</a> &nbsp; <a href='+'"'+footSet[20]+'"'+'>XTList</a> &nbsp; <a href='+'"'+footSet[21]+'"'+'>Mobilist</a> &nbsp; <a href='+'"'+footSet[22]+'"'+'>Charrua Mobilist</a> &nbsp; <a href='+'"'+footSet[23]+'"'+'>WAP2 TopZ</a> &nbsp; <a href='+'"'+footSet[24]+'"'+'>PornToplistcreator</a> &nbsp; <a href='+'"'+footSet[25]+'"'+'>9ADS TopZ</a> &nbsp; <a href='+'"'+footSet[26]+'"'+'>HotGirls mobiLIST</a> &nbsp; <a href='+'"'+footSet[27]+'"'+'>XNXX TopListCreator</a> &nbsp; <a href='+'"'+footSet[28]+'"'+'>MostList TopHits</a> &nbsp; <a href='+'"'+footSet[29]+'"'+'>iTech topZ</a> &nbsp; <a href='+'"'+footSet[30]+'"'+'>MP4x MobiList Mobi</a> &nbsp; <a href='+'"'+footSet[31]+'"'+'>adZlist TopZ MOBI</a> &nbsp; <a href='+'"'+footSet[32]+'"'+'>adZmobi topZ mobi</a></p>');
+}
+
+// counter
+function disp_count() {
+	document.write('<p><iframe src='+'"'+'http://hitch.unaux.com/count.php'+'"'+' width='+'"'+'98'+'"'+' height='+'"'+'37'+'"'+' style='+'"'+'border: none'+'"'+'></iframe></p>');
+}
+
+// footer $1
+function members(){
+	document.write('<p>Hitch &copy; 2018 &nbsp;-&nbsp; For support, send mail to <a href='+'"'+'mailto:redist@gmx.es'+'"'+'>redist@gmx.es</a></p>');
+}
+
+// ad top
+function adtop() {
+	var min = 1;
+	var max = 1;
+	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
+
+	if (get_seed == 1)
+		{
+			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.728x90.2.gif'+'"'+' width='+'"'+'728'+'"'+' height='+'"'+'90'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a><br /><a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'>Get paid to share your links!</a></p>');
+		}
+}
+
+// ad left
+function adleft() {
+	var min = 1;
+	var max = 1;
+	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
+
+	if (get_seed == 1)
+		{
+			document.write('<p align='+'"'+'left'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.468x60.5.gif'+'"'+' width='+'"'+'468'+'"'+' height='+'"'+'60'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a><br /><a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'>Get paid to share your links!</a></p>');
+		}
+}
+
+// ad bottom
+function adbottom() {
+	var min = 1;
+	var max = 1;
+	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
+	
+
+	if (get_seed == 1)
+		{
+			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.300x250.1.gif'+'"'+' width='+'"'+'300'+'"'+' height='+'"'+'250'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a></p>');
+		}
+}

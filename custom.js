b42940449846dead2b53e5740dc9475a9c8dc642
ualhalla <41804315+ualhalla@users.noreply.github.com>
2018-11-05 02:03:33 +0800

    Add files via upload

--- custom.js
+++ custom.js
@@ -11,8 +11,34 @@ function foot() {
 		footSet[8] = "http://downloads.mobilist.mobi/in/90769";
 		footSet[9] = "http://charrua.mobilist.mobi/in/90770";
 		footSet[10] = "http://wap2.topz.mobi/in/90771";
+		footSet[11] = "http://adzline.topz.mobi/in/90772";
+		footSet[12] = "http://porn.toplistcreator.eu/in.php?nr=14982";
+		footSet[13] = "http://9ads.topz.mobi/in/90773";
+		footSet[14] = "http://hotgirls.mobilist.mobi/in/90774";
+		footSet[15] = "http://xnxx.toplistcreator.eu/in.php?nr=14983";
+		footSet[16] = "http://mostlist.tophits.mobi/in/90775";
+		footSet[17] = "http://itech.topz.mobi/in/90776";
+		footSet[18] = "http://mp4x.mobilist.mobi/in/90777";
+		footSet[19] = "http://adzlist.topz.mobi/in/90778";
+		footSet[20] = "http://itech.topzone.mobi/in/90779";
+		footSet[21] = "http://xtlist.com/in/115254";
+		footSet[22] = "http://sexlist.mobi/in/115255";
+		footSet[23] = "http://mobpath.com/in/115256";
+		footSet[24] = "http://sextop.xxx/in/115257";
+		footSet[25] = "http://wapfun.eu/in/115258";
+		footSet[26] = "http://sextop.co/in/115259";
+		footSet[27] = "http://x2list.com/in/115260";
+		footSet[28] = "http://kickme.mobi/in/115261";
+		footSet[29] = "http://waptop.eu/in/115262";
+		footSet[30] = "http://mtop.mobi/in/115263";
+		footSet[31] = "http://erolist.net/in/115264";
+		footSet[32] = "http://toplistx.com/in/115265";
+		footSet[33] = "http://top.mobilist.mobi/in/90780";
+		footSet[34] = "http://loland.topz.mobi/in/90781";
+		footSet[35] = "http://top.andrew-lviv.net/?uid=17379";
+		footSet[36] = "http://adzmobi.topz.mobi/in/90782";
 
-	document.write('<p style='+'"'+'text-align: left; font-family: verdana; font-size: 11px'+'"'+'>Partners: <a href='+'"'+footSet[1]+'"'+'>name mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[2]+'"'+'>wapDAY TopZ MOBI</a> &nbsp; <a href='+'"'+footSet[3]+'"'+'>ADZwap TopZ MOBI</a> &nbsp; <a href='+'"'+footSet[4]+'"'+'>WAP TopListCreator EU</a> &nbsp; <a href='+'"'+footSet[5]+'"'+'>OnlyHits mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[6]+'"'+'>WAP topZ MOBI</a> &nbsp; <a href='+'"'+footSet[7]+'"'+'>XXX mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[8]+'"'+'>DOWNLOADS mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[9]+'"'+'>Charrua mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[10]+'"'+'>WAP2 topZ MOBI</a></p>');
+	document.write('<p style='+'"'+'text-align: left; font-family: verdana; font-size: 11px'+'"'+'>Partners: <a href='+'"'+footSet[1]+'"'+'>name mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[2]+'"'+'>wapDAY TopZ MOBI</a> &nbsp; <a href='+'"'+footSet[3]+'"'+'>ADZwap TopZ MOBI</a> &nbsp; <a href='+'"'+footSet[4]+'"'+'>WAP TopListCreator EU</a> &nbsp; <a href='+'"'+footSet[5]+'"'+'>OnlyHits mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[6]+'"'+'>WAP topZ MOBI</a> &nbsp; <a href='+'"'+footSet[7]+'"'+'>XXX mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[8]+'"'+'>DOWNLOADS mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[9]+'"'+'>Charrua mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[10]+'"'+'>WAP2 topZ MOBI</a> &nbsp; <a href='+'"'+footSet[11]+'"'+'>AdzLine topZ MOBI</a> &nbsp; <a href='+'"'+footSet[12]+'"'+'>Porn ToplistCreator EU</a> &nbsp; <a href='+'"'+footSet[13]+'"'+'>9ADS topZ MOBI</a> &nbsp; <a href='+'"'+footSet[14]+'"'+'>HOTgirls mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[15]+'"'+'>XNXX ToplistCreator EU</a> &nbsp; <a href='+'"'+footSet[16]+'"'+'>MostList topHits MOBI</a> &nbsp; <a href='+'"'+footSet[17]+'"'+'>iTech topZ MOBI</a> &nbsp; <a href='+'"'+footSet[18]+'"'+'>MP4x mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[19]+'"'+'>adZlist topZ MOBI</a> &nbsp; <a href='+'"'+footSet[20]+'"'+'>iTech topZone MOBI</a> &nbsp; <a href='+'"'+footSet[21]+'"'+'>xtList</a> &nbsp; <a href='+'"'+footSet[22]+'"'+'>sexList MOBI</a> &nbsp; <a href='+'"'+footSet[23]+'"'+'>MobPath</a> &nbsp; <a href='+'"'+footSet[24]+'"'+'>sexTop XXX</a> &nbsp; <a href='+'"'+footSet[25]+'"'+'>WAPfun EU</a> &nbsp; <a href='+'"'+footSet[26]+'"'+'>sexTop CO</a> &nbsp; <a href='+'"'+footSet[27]+'"'+'>x2List</a> &nbsp; <a href='+'"'+footSet[28]+'"'+'>KickMe MOBI</a> &nbsp; <a href='+'"'+footSet[29]+'"'+'>WAPtop EU</a> &nbsp; <a href='+'"'+footSet[30]+'"'+'>mTop MOBI</a> &nbsp; <a href='+'"'+footSet[31]+'"'+'>EroList</a> &nbsp; <a href='+'"'+footSet[32]+'"'+'>topListX</a> &nbsp; <a href='+'"'+footSet[33]+'"'+'>Top mobiList MOBI</a> &nbsp; <a href='+'"'+footSet[34]+'"'+'>Loland topZ MOBI</a> &nbsp; <a href='+'"'+footSet[35]+'"'+'>Top-Rating</a> &nbsp; <a href='+'"'+footSet[36]+'"'+'>AdzMobi topZ MOBI</a></p>');
 }
 
 // counter

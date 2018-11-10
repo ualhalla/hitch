818cab4feaafa19cf8b21fe687ae0d362c7b01f0
ualhalla <41804315+ualhalla@users.noreply.github.com>
2018-11-10 11:03:41 +0800

    Add files via upload

--- custom.js
+++ custom.js
@@ -60,72 +60,36 @@ function members(){
 // ad top
 function adtop() {
 	var min = 1;
-	var max = 4;
+	var max = 1;
 	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
 
 	if (get_seed == 1)
 		{
 			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.728x90.2.gif'+'"'+' width='+'"'+'728'+'"'+' height='+'"'+'90'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a><br /><a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'>Get paid to share your links!</a></p>');
 		}
-	if (get_seed == 2)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://affili.st/322366'+'"'+' class='+'"'+'btn btn-danger'+'"'+'><img  src='+'"'+'https://aff-aff.advertica-cdn.com/728/90/130.gif'+'"'+'/></a></p>');
-		}
-	if (get_seed == 3)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/6735e931f6/5766e48282/?placementName=default&width=728&height=90'+'"'+'></script></p>');
-		}
-	if (get_seed == 4)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/6735e931f6/5766e48282/?placementName=default&width=728&height=90'+'"'+'></script></p>');
-		}
 }
 
 // ad left
 function adleft() {
 	var min = 1;
-	var max = 4;
+	var max = 1;
 	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
 
 	if (get_seed == 1)
 		{
 			document.write('<p align='+'"'+'left'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.468x60.5.gif'+'"'+' width='+'"'+'468'+'"'+' height='+'"'+'60'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a><br /><a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'>Get paid to share your links!</a></p>');
 		}
-	if (get_seed == 2)
-		{
-			document.write('<p align='+'"'+'left'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://affili.st/322366'+'"'+' class='+'"'+'btn btn-danger'+'"'+'><img  src='+'"'+'https://aff-aff.advertica-cdn.com/468/60/49.jpg'+'"'+'/></a></p>');
-		}
-	if (get_seed == 3)
-		{
-			document.write('<p align='+'"'+'left'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/2b2614e689/21b429a008/?placementName=default&width=468&height=60'+'"'+'></script></p>');
-		}
-	if (get_seed == 4)
-		{
-			document.write('<p align='+'"'+'left'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/2b2614e689/21b429a008/?placementName=default&width=468&height=60'+'"'+'></script></p>');
-		}
 }
 
 // ad bottom
 function adbottom() {
 	var min = 1;
-	var max = 4;
+	var max = 1;
 	var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
 	
 
 	if (get_seed == 1)
 		{
 			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://adult.xyz/?id=17792889'+'"'+'><img border='+'"'+'0'+'"'+' src='+'"'+'https://cdn.adult.xyz/images/banners/adult.300x250.1.gif'+'"'+' width='+'"'+'300'+'"'+' height='+'"'+'250'+'"'+' title='+'"'+'Adult.xyz - shorten links and earn money!'+'"'+' /></a></p>');
-		}
-	if (get_seed == 2)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<a href='+'"'+'https://affili.st/322366'+'"'+' class='+'"'+'btn btn-danger'+'"'+'><img  src='+'"'+'https://aff-aff.advertica-cdn.com/300/250/145.gif'+'"'+'/></a></p>');
-		}
-	if (get_seed == 3)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/d9f5ac4fa9/9e158a8b54/?placementName=default&width=300&height=250'+'"'+'></script></p>');
-		}
-	if (get_seed == 4)
-		{
-			document.write('<p align='+'"'+'center'+'"'+' style='+'"'+'font-family: verdana; font-size: 10px;'+'"'+'>Ad&nbsp;<script type='+'"'+'text/javascript'+'"'+' src='+'"'+'//adwb.net/d9f5ac4fa9/9e158a8b54/?placementName=default&width=300&height=250'+'"'+'></script></p>');
 		}
 }
